1af1c721966f1a475c67551247fba3c9353cf35c
sneaksnake <sneaksnake@posteo.uk>
2016-04-07 13:18:59 +0200

    Save and restore progress of players (this closes #1)

--- bot.py
+++ bot.py
@@ -2,7 +2,8 @@ import json
 import logging
 import os
 
-import telegram
+import redis
+import telegram.ext
 
 from dfrotz import DFrotz
 
@@ -74,6 +75,7 @@ class Z5Bot:
   def __init__(self):
     self.__class__.instances.append(self)
     self.chats = []
+    self.redis = None
 
   @classmethod
   def get_instance_or_create(self):
@@ -84,6 +86,9 @@ class Z5Bot:
       instance = Z5Bot()
       logging.debug('Created new Z5Bot instance: %r' % instance)
     return instance
+
+  def add_redis(self, redis):
+    self.redis = redis
 
   def add_chat(self, chat):
     self.chats.append(chat)
@@ -129,15 +134,40 @@ def cmd_select(bot, update):
       chat.set_story(Story.get_instance_by_abbrev(story.abbrev))
       z5bot.add_chat(chat)
       bot.sendMessage(update.message.chat_id, text='Starting "%s"...' % story.name)
+      notice  = 'Your progress will be saved automatically.'
+      bot.sendMessage(update.message.chat_id, text=notice)
+      if z5bot.redis is None:
+        # TODO: make it configurable. for now on, just edit this file
+        r = redis.StrictRedis(host='192.168.178.53', port=6379, db=1)
+        z5bot.add_redis(r)
       bot.sendMessage(update.message.chat_id, text=z5bot.receive(update.message.chat_id))
+      if z5bot.redis.exists(update.message.chat_id):
+        notice  = 'Some progress already exists. Use /load to restore it '
+        notice += 'or /clear to reset your recorded actions.'
+        bot.sendMessage(update.message.chat_id, text=notice)
       return
 
   bot.sendMessage(update.message.chat_id, text=text)
 
 def on_message(bot, update):
+  logging.info('@%s[%d] sent: %r' % (
+    update.message.from_user.username,
+    update.message.from_user.id,
+    update.message.text[:30])
+  )
+
+  if update.message.text.strip() == 'load':
+    text = 'Please use /load.'
+    bot.sendMessage(update.message.chat_id, text=text)
+    return
+
+  if update.message.text.strip() == 'save':
+    text = 'Your progress is being saved automatically. But /load is available.'
+    bot.sendMessage(update.message.chat_id, text=text)
+    return
+
   z5bot = Z5Bot.get_instance_or_create()
 
-  logging.info('@%s[%d] sent: %r' % (update.message.from_user.username, update.message.from_user.id, update.message.text[:30]))
 
   id = update.message.chat_id
   chat = Chat.get_instance_or_create(id)
@@ -147,11 +177,71 @@ def on_message(bot, update):
     bot.sendMessage(update.message.chat_id, text=text)
     return
 
+  # here, stuff is sent to the interpreter
+  z5bot.redis.rpush(update.message.chat_id, update.message.text)
   z5bot.process(id, update.message.text)
 
   received = z5bot.receive(id)
-  logging.info('Answering @%s[%d]: %r' % (update.message.from_user.username, update.message.from_user.id, received[:30]))
+  logging.info('Answering @%s[%d]: %r' % (
+    update.message.from_user.username,
+    update.message.from_user.id,
+    received[:30])
+  )
   bot.sendMessage(update.message.chat_id, text=received)
+  if 'return' in received.lower() or 'enter' in received.lower():
+    notice = '(Note: You are able to do that by typing /enter.)'
+    bot.sendMessage(update.message.chat_id, text=received)
+
+def cmd_load(bot, update):
+  z5bot = Z5Bot.get_instance_or_create()
+
+  chat = Chat.get_instance_or_create(update.message.chat_id)
+  if chat.story is None:
+    text = 'You have to select a game first.'
+    bot.sendMessage(update.message.chat_id, text=text)
+    return
+
+  if not z5bot.redis.exists(update.message.chat_id):
+    text = 'There is no progress to load.'
+    bot.sendMessage(update.message.chat_id, text=text)
+    return
+
+  text = 'Restoring %d messages. Please wait.' % z5bot.redis.llen(update.message.chat_id)
+  bot.sendMessage(update.message.chat_id, text=text)
+
+  messages = z5bot.redis.lrange(update.message.chat_id, 0, -1)
+
+  for index, message in enumerate(messages):
+    z5bot.process(update.message.chat_id, message.decode('utf-8'))
+    if index == len(messages)-2:
+      z5bot.receive(update.message.chat_id) # clear buffer
+  bot.sendMessage(update.message.chat_id, text='Done.')
+  bot.sendMessage(update.message.chat_id, text=z5bot.receive(update.message.chat_id))
+
+def cmd_clear(bot, update):
+  z5bot = Z5Bot.get_instance_or_create()
+
+  if not z5bot.redis.exists(update.message.chat_id):
+    text = 'There is no progress to clear.'
+    bot.sendMessage(update.message.chat_id, text=text)
+    return
+
+  text = 'Deleting %d messages. Please wait.' % z5bot.redis.llen(update.message.chat_id)
+  bot.sendMessage(update.message.chat_id, text)
+
+  z5bot.redis.delete(update.message.chat_id)
+  bot.sendMessage(update.message.chat_id, text='Done.')
+
+def key_enter(bot, update):
+  z5bot = Z5Bot.get_instance_or_create()
+  chat = Chat.get_instance_or_create(update.message.chat_id)
+  if chat.story is None:
+    return
+  command = '' # \r\n is automatically added by the Frotz abstraction layer
+  z5bot.redis.rpush(update.message.chat_id, command)
+  z5bot.process(update.message.chat_id, command)
+  bot.sendMessage(update.message.chat_id, text=z5bot.receive(update.message.chat_id))
+
 
 def on_error(bot, update, error):
   logger = logging.getLogger(__name__)
@@ -173,13 +263,19 @@ if __name__ == '__main__':
 
   print(Story.instances)
 
-  updater = telegram.Updater(api_key)
+  updater = telegram.ext.Updater(api_key)
+
+  job_queue = updater.job_queue
 
   dispatcher = updater.dispatcher
-
   dispatcher.addTelegramCommandHandler('start', cmd_start)
   dispatcher.addTelegramCommandHandler('select', cmd_select)
+  dispatcher.addTelegramCommandHandler('load', cmd_load)
+  dispatcher.addTelegramCommandHandler('clear', cmd_clear)
+  dispatcher.addTelegramCommandHandler('enter', key_enter)
   dispatcher.addTelegramMessageHandler(on_message)
   dispatcher.addErrorHandler(on_error)
+
+
   updater.start_polling()
   updater.idle()
